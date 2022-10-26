7b9d5b83a89efcc7e44edb277a299af3df6194b8
rakeshwasnik <rbwasnik99@gmail.com>
2022-10-26 14:47:06 +0530

    .

--- src/App.js
+++ src/App.js
@@ -99,6 +99,41 @@ function reducer(state, { type, payload }) {
 }
 
 
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  switch (operation) {
+    case "+":
+      computation = prev + current
+      break
+    case "-":
+      computation = prev - current
+      break
+    case "*":
+      computation = prev * current
+      break
+    case "÷":
+      computation = prev / current
+      break
+  }
+
+  return computation.toString()
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+function formatOperand(operand) {
+  if (operand == null) return
+  const [integer, decimal] = operand.split(".")
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
+
+
 function App() {
 
   const [{previousOperand, currentOperand, operation}, dispatch] = useReducer(reducer, {})
